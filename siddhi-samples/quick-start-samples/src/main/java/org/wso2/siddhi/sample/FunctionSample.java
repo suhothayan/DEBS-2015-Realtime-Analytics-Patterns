cf842a8a7797884be1efa4a1420ed3a9cf5bd445
suhothayan <suhothayan@gmail.com>
2015-06-27 12:31:51 +0530

    adding comments to siddhi sample

--- siddhi-samples/quick-start-samples/src/main/java/org/wso2/siddhi/sample/FunctionSample.java
+++ siddhi-samples/quick-start-samples/src/main/java/org/wso2/siddhi/sample/FunctionSample.java
@@ -26,14 +26,21 @@ import org.wso2.siddhi.core.util.EventPrinter;
 public class FunctionSample {
     public static void main(String[] args) throws InterruptedException {
 
-        // Create Siddhi Manager
+        // Creating Siddhi Manager
         SiddhiManager siddhiManager = new SiddhiManager();
 
-        String executionPlan = "define stream cseEventStream (symbol string, price1 float, price2 float, volume long , quantity int);"
-                + "@info(name = 'query1') from cseEventStream select symbol, coalesce(price1,price2) as price,quantity insert into outputStream;";
+        String executionPlan = "" +
+                "define stream cseEventStream (symbol string, price1 float, price2 float, volume long , quantity int);" +
+                "" +
+                "@info(name = 'query1') " +
+                "from cseEventStream " +
+                "select symbol, coalesce(price1,price2) as price, quantity " +
+                "insert into outputStream;";
 
+        //Generating runtime
         ExecutionPlanRuntime executionPlanRuntime = siddhiManager.createExecutionPlanRuntime(executionPlan);
 
+        //Adding callback to retrieve output events from query
         executionPlanRuntime.addCallback("query1", new QueryCallback() {
             @Override
             public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
@@ -41,17 +48,22 @@ public class FunctionSample {
             }
         });
 
+        //Retrieving InputHandler to push events into Siddhi
+        InputHandler inputHandler = executionPlanRuntime.getInputHandler("cseEventStream");
+
+        //Starting event processing
         executionPlanRuntime.start();
 
-        System.out.println("coalesce function");
-        InputHandler inputHandler = executionPlanRuntime.getInputHandler("cseEventStream");
+        //Sending events to Siddhi
         inputHandler.send(new Object[]{"WSO2", 50f, 60f, 60l, 6});
         inputHandler.send(new Object[]{"WSO2", 70f, null, 40l, 10});
         inputHandler.send(new Object[]{"WSO2", null, 44f, 200l, 56});
         Thread.sleep(100);
 
+        //Shutting down the runtime
         executionPlanRuntime.shutdown();
 
+        //Shutting down Siddhi
         siddhiManager.shutdown();
 
     }
